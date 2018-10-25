79e55d674a98abbf8b85af4cc75de5724ba888a0
Seth L. Blumberg <sethb@pobox.com>
2018-10-25 05:37:14 -0400

    add Object.fromEntries() shim

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
+/* eslint no-console: off */
 import yargs from 'yargs';
 
-const y = yargs
-  .scriptName('ven2')
-  .usage('Usage: $0 <command> [options]')
-  .strict()
-  .demandCommand(1)
-  .commandDir('commands')
-  .options({
-    debug: {
-      description: 'Debugging output',
-      alias: 'd',
-      hidden: true,
-      nargs: 0,
-      type: 'boolean',
-    },
-  })
-  .help()
-  .version()
-  .alias({
-    help: 'h',
-    version: 'V',
-  });
+// Babel 7 doesn't yet have a plugin/polyfill for Object.fromEntries even though it's at
+// stage 3 of the proposal process...I thought all Stage 3 and lower proposals were supposed to
+// be covered by preset-env?
+const p = Object.fromEntries
+  ? Promise.resolve()
+  : import('object.fromentries').then((x) => x.default.shim());
 
-if (!y.argv._.length) {
-  y.showHelp('log');
-}
+p.then(() => {
+  const y = yargs
+    .scriptName('ven2')
+    .usage('Usage: $0 <command> [options]')
+    .strict()
+    .commandDir('commands')
+    .options({
+      debug: {
+        description: 'Debugging output',
+        alias: 'd',
+        hidden: true,
+        nargs: 0,
+        type: 'boolean',
+      },
+    })
+    .help()
+    .version()
+    .alias({
+      help: 'h',
+      version: 'V',
+    });
+
+  if (!y.argv._.length) {
+    y.showHelp('log');
+  }
+})
+  .catch(console.error);
--- test/setup.js
+++ test/setup.js
@@ -3,4 +3,13 @@ import chaiAsPromised from 'chai-as-promised';
 
 chai.use(chaiAsPromised);
 
-export default chai.expect;
+export const expect = chai.expect;
+
+// Babel 7 doesn't yet have a plugin/polyfill for Object.fromEntries even though it's at
+// stage 3 of the proposal process...I thought all Stage 3 and lower proposals were supposed to
+// be covered by preset-env?
+export function shim () {
+  return Object.fromEntries
+    ? Promise.resolve()
+    : import('object.fromentries').then((x) => x.default.shim());
+}
