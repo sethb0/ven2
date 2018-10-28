df20e8ed7f3465146da614dacde28d854d29ae53
Seth L. Blumberg <sethb@pobox.com>
2018-10-28 02:15:01 -0400

    simplify error output froo 'validate' command

--- src/handlers/validate.js
+++ src/handlers/validate.js
@@ -1,15 +1,13 @@
 /* eslint no-console: off */
 import Ajv from 'Ajv';
 import ajvMergePatch from 'ajv-merge-patch';
+import fs from 'fs';
 import { load as loadYaml } from '@sethb0/yaml-utils';
 import path from 'path';
 import { promisify } from 'util';
-import fs from 'fs';
 
 const readDir = promisify(fs.readdir);
-const readFile = promisify(fs.readFile);
 
-const ITEM_SCHEMA_ID = 'urn:mfllc:venator:schema:master-item-schema';
 const ROOT_SCHEMA_IDS = [
   'https://venator.sharpcla.ws/schema/character.json',
   'https://venator.sharpcla.ws/schema/charm.json',
@@ -18,41 +16,39 @@ const ROOT_SCHEMA_IDS = [
 
 export default async function validate (argv) {
   const schemaDir = path.resolve(__dirname, '..', 'schemata');
-  const files = (await readDir(schemaDir)).map((f) => path.join(schemaDir, f));
-  const schemata = await Promise.all([
-    ...files.filter((f) => f.endsWith('.json')).map(loadJson),
-    ...files.filter((f) => f.endsWith('.yml')).map(loadYaml),
-    // ignore other files (shouldn't be any)
-    {
-      $id: ITEM_SCHEMA_ID,
-      oneOf: ROOT_SCHEMA_IDS.map((id) => ({ $ref: id })),
-    },
-  ]);
+  const schemata = (await readDir(schemaDir)).map((f) => loadYaml(path.join(schemaDir, f)));
+  // if there are any JSON files, we take advantage of the fact that YAML is a superset of JSON
   const ajv = new Ajv({
     $data: true,
     jsonPointers: true,
     unicode: false,
     extendRefs: 'fail',
-    schemas: schemata,
+    schemas: await Promise.all(schemata),
   });
   ajvMergePatch(ajv);
   const masterSchema = {
-    oneOf: [{ $ref: ITEM_SCHEMA_ID }, { type: 'array', items: { $ref: ITEM_SCHEMA_ID } }],
+    oneOf: [
+      { type: 'array', maxItems: 0 },
+    ].concat(...ROOT_SCHEMA_IDS.map((id) => [
+      { $ref: id },
+      { type: 'array', items: { $ref: id }, minItems: 1 },
+    ])),
   };
   const validator = ajv.compile(masterSchema);
   for (const { filename, data } of await argv.files) {
+    // we don't parallelize these because validator isn't reentrant and because we don't want
+    // the output to be interleaved
     if (validator(data)) {
       console.log(`${filename} is valid`);
     } else {
-      const { errors } = validator;
-      console.log(
-        `${filename} had ${errors.length} validation error${errors.length === 1 ? '' : 's'}:`,
-      );
-      errors.forEach((e) => console.log(e));
+      console.log(`${filename} failed validation`);
+      if (argv.debug) {
+        for (const e of validator.errors) {
+          console.log(e);
+        }
+      } else {
+        console.log(validator.errorsText());
+      }
     }
   }
-}
-
-function loadJson (f) {
-  return readFile(f, 'utf8').then(JSON.parse);
 }
